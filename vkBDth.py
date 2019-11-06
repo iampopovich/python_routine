7f2c282a75df85e8bc4628d57737a59d6cf5d99a
Alex Popovich <iampopovich@gmail.com>
2019-11-06 23:32:15 +0300

    fuk da vk api

--- vkBDth.py
+++ vkBDth.py
@@ -1,14 +1,28 @@
+import sys 
 import vk
-import sys
+import requests
+import re
+from bs4 import BeautifulSoup
+
+def getToken():
+	url = ""
+	token = requests.get(url).text
+	return token 
 
 
+def scrap(session):
+	# token = "9a33a4b06f9a6fe0d099a339a33f4b0c7f0fb272439a18c730b4568"
+	session = vk.Session()
+	vkAPI = vk.API(session,scope='users')
+	friendIDs = vkAPI.friends.get()
 
-def scrap(token)
-	session = 
-
-
-def main(token):
-	scrap(token)
+def main(args):
+	login = ""
+	password = ""
+	appID = ""
+	# token = getToken()
+	session = getAuthSession(login, password, appID)
+	scrap(session)
 
 
 if __name__ == "__main__":
