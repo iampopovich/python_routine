14275a0ea155fb2d934b71615ca37cfcd674a4f9
Alex Popovich <iampopovich@gmail.com>
2019-11-07 20:34:31 +0300

    update lol

--- curScrap.py
+++ curScrap.py
@@ -1,2 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+
+
+def getAllLinks():
+	return links
+
+
+def main():
+	#url = 
+	allLinks = getAllLinks()
+	return 0
+
+if __name__ == "__main__":
+	main()
