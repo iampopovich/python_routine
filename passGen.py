33b79d121709d9ae178008cc781346ca408300e1
Alex <iampopovich@gmail.com>
2019-12-16 22:30:36 +0300

    v0.1.0
    stable version
    features -p "punctuation" and -w "whitespace"  will be added later

--- passGen.py
+++ passGen.py
@@ -1,31 +1,45 @@
-#ver:0.0.2
+#ver:0.1.0
 import string
 import random
 import sys
 import os
 import argparse
 
-def genPasswordList():
-	pass
-
 def initArgParser():
 	p = argparse.ArgumentParser(description = "Select some options: ")
-	p.add_argument("-f", metavar = "FILE", help = "generate password file in current directory")
-	p.add_argument("-n", metavar = "NUMBER", help = "pick length of password")
-	p.add_argument("-s", metavar = "SYMBOL", help = "include uppercase/lowercase symbol in password chars")
-	p.add_argument("-l", metavar = "LWC", type = bool, default = False, help = "include lowercase symbol in password chars") 
-	p.add_argument("-u", metavar = "UPC", type = bool, default = False, help = "include uppercase symbol in password chars" ) 
-	p.add_argument("-N", metavar = "NUM", type = bool, default = False, help = "include numbers in password chars") 
-	p.add_argument("-p", metavar = "PUN", type = bool, default = False, help = "include punctuation in password chars") 
-	p.add_argument("-w", metavar = "WSP", type = bool, default = False, help = "include whitespace in password chars") 
+	p.add_argument("-n", "--NUMBER", type = int, default = 10, required = True, help = "pick a length of password")
+	p.add_argument("-r", "--ROW", type = int, default = 1, required = False, help = "pick a number to generate few passwords")
+	p.add_argument("-s", "--SYMBOL", default = False, required = False, action = "store_true", help = "include uppercase/lowercase symbol in password chars")
+	p.add_argument("-l", "--LWC", default = False, required = False, action = "store_true", help = "include lowercase symbol in password chars") 
+	p.add_argument("-u", "--UPC", default = False, required = False, action = "store_true", help = "include uppercase symbol in password chars" ) 
+	p.add_argument("-d", "--DGT", default = False, required = False, action = "store_true", help = "include digits in password chars") 
+	# p.add_argument("-p", "--PUN", default = False, required = False, action = "store_true", help = "include punctuation in password chars") 
+	# p.add_argument("-w", "--WSP", default = False, required = False, action = "store_true", help = "include whitespace in password chars") 
 	return p
 
 def genPassword(params):
-	pass
+	pwds = []
+	charset = ""
+	if params.SYMBOL:
+		charset += string.ascii_letters
+	else:
+		if params.LWC : charset += string.ascii_lowercase 
+		elif params.UPC : charset += string.ascii_uppercase
+		else: charset += string.ascii_letters
+	if params.DGT : charset+= string.digits
+	# if params.PUN : charset+= string.punctuation
+	# if params.WSP: charset+= string.whitespace
+	for i in range(params.ROW):
+		pwd = "".join(random.choice(charset) for i in range(params.NUMBER))
+		pwds.append(pwd)
+	print("\n".join(pwds))
+	return None
 
 def main():
 	parser = initArgParser()
 	args = parser.parse_args()
+	genPassword(args)
+	return None
 
 if __name__ == "__main__":
 	main()
