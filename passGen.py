9ae4f595ee8e3f9b08d7cc8ce690395d909c4d8d
Alex <iampopovich@gmail.com>
2019-12-16 19:41:37 +0300

    init commit

---
+++ passGen.py
@@ -0,0 +1,21 @@
+#ver:0.0.1
+import string
+import random
+import sys
+import os
+import argparse
+
+def genPasswordList():
+	pass
+
+def initArgParser():
+	pass
+
+def genPassword(params):
+	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
