181a05d97a6cf916ff2f7f8383c707bfd744c44b
Alex <iampopovich@gmail.com>
2020-04-04 21:43:02 +0300

    1_requests.py чекаю, что может стдлиб для написания рест тестов_

--- 1_requests.py
+++ 1_requests.py
@@ -1,4 +1,6 @@
 import requests
+import re
+import inspect
 
 # #create a request object 
 # r_get = requests.get('https://www.google.com')
@@ -16,8 +18,12 @@ import requests
 
 #response time measurement 
 def check_time_response_get(url, timeout):
-	r = requests.get(url)
-	return r.elapsed.total_seconds() <= timeout/1000
+	try:
+		r = requests.get(url)
+		return r.elapsed.total_seconds() <= timeout/1000
+	except Exception as ex:
+		return 'Error in {}: {}'.format(inspect.stack()[0][3], ex)
+
 
 def check_time_response_post(url, timeout):
 	r = requests.post(url)
@@ -25,12 +31,26 @@ def check_time_response_post(url, timeout):
 
 #response status code 
 def check_response_status(url, status):
-	r = requests.get(url)
-	return r.status_code == status
+	try:
+		r = requests.get(url)
+		return r.status_code == status
+	except Exception as ex:
+		return 'Error in {}: {}'.format(inspect.stack()[0][3], ex)
+
+#response text contains something brrr
+def check_response_contains(url, text):
+	try:
+		r = requests.get(url)
+		contains = re.search(text,r.text)
+		return bool(contains.group(0))
+	except Exception as ex:
+		return 'Error in {}: {}'.format(inspect.stack()[0][3], ex)
+
+test_get = check_time_response_get('https://www.goog0le.com', 300)
+test_status = check_response_status('https://www.goo--gle.com', 300)
+test_contains = check_response_contains('https://www.google.com', 'go0gle')
 
 
-test_get = check_time_response_get('https://www.google.com', 300)
-test_status = check_response_status('https://www.google.com', 300)
-
-print(test_get)
-print(test_status)
+print(test_get, '\n')
+print(test_status,'\n')
+print(test_contains,'\n')
