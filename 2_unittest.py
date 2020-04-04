3222aca890b6907a33f12b64e5d3e61323d78a5a
Alex <iampopovich@gmail.com>
2020-04-05 01:44:27 +0300

    2_unittest.py теперь тесты стартуют. но обработку эксепшнов надо будет как-то колхозить все-равно. RTFM

--- 2_unittest.py
+++ 2_unittest.py
@@ -3,12 +3,13 @@ import re
 import inspect
 import unittest
 
-#response time measurement 
 class TestREST(unittest.TestCase):
-	url = 'https://www.goog0le.com'
-	timeout = 300
-	status = 200
-	text = 'goog0le'
+
+	def setUp(self):
+		self.url = 'https://www.goog0le.com'
+		self.timeout = 300
+		self.status = 200
+		self.text = 'goog0le'
 
 	def check_time_response_get(self):
 		r = requests.get(self.url)
@@ -29,6 +30,9 @@ class TestREST(unittest.TestCase):
 		contains = re.search(self.text,r.text)
 		self.assertEqual(bool(contains.group(0), True))
 
+	def runTest(self):
+		self.check_response_status()
+		self.check_response_contains()
 # test_get = check_time_response_get('https://www.goog0le.com', 300)
 # test_status = check_response_status('https://www.goo--gle.com', 300)
 # test_contains = check_response_contains('https://www.google.com', 'go0gle')
@@ -38,4 +42,4 @@ class TestREST(unittest.TestCase):
 # print(test_contains,'\n')
 
 if __name__ ==  '__main__':
-	unittest.main()
+	unittest.TextTestRunner().run(TestREST())
