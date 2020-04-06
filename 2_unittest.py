027393cf998c0a1b3d290615d31baac9fb7d8de9
Alex <iampopovich@gmail.com>
2020-04-07 01:59:20 +0300

    2_unittest.py вынес настройки для кейсов в глобальный скоуп. добавил еще тесткейс. оставил в сетапе тесткейса возможность кастома парметров. \!МБ ЭТО ПЛОХАЯ ПРАКТИКА\!

--- 2_unittest.py
+++ 2_unittest.py
@@ -3,45 +3,43 @@ import re
 import inspect
 import unittest
 
-URL = 'https://www.google.com'
-TIMEOUT = 300
+URL_GET = 'https://www.google.com'
+URL_POST = ''
+TIMEOUT_GET = 300
+TIMEOUT_POST = 200
 STATUS = 200
 TEXT = 'google'
 
 class testResponseStatus(unittest.TestCase): #remember pep-8 and refactor it !
 
-	def setUp(self):
-		self.url = URL
-		self.timeout = TIMEOUT
-		self.status = STATUS
-		self.text = TEXT
-
 	def test_response_status(self):
-		r = requests.get(self.url)
-		self.assertEqual(r.status_code, self.status)
+		r = requests.get(URL_GET)
+		self.assertEqual(r.status_code, STATUS)
+
+class testResponseoContains(unittest.TestCase): #remember pep-8 and refactor it !
 
 	def test_response_contains(self):
-		r = requests.get(self.url)
-		contains = re.search(self.text,r.text)
+		r = requests.get(URL_GET)
+		contains = re.search(TEXT,r.text)
 		self.assertIsNotNone(contains)
 
 class testResponseTime(unittest.TestCase):
-# Remember DRY and refactore it !
 
-	def setUp(self):
-		self.url = URL
-		self.timeout = TIMEOUT
+	def setUp(self): # Оставил для возможности кастомизации тесткейса 
+		self.url_get = URL_GET
+		self.url_post = URL_POST
+		self.timeout_get = TIMEOUT_GET
+		self.timeoit_post = TIMEOUT_POST
 		self.status = STATUS
 		self.text = TEXT
 
-
 	def test_time_response_get(self):
-		r = requests.get(self.url)
-		self.assertLessEqual(r.elapsed.total_seconds(), self.timeout/1000 )
+		r = requests.get(self.url_get)
+		self.assertLessEqual(r.elapsed.total_seconds(), self.timeout_get/1000 )
 
 	def test_time_response_post(self):
-		r = requests.post(self.url)
-		self.assertLessEqual(r.elapsed.total_seconds(),self.timeout/1000)
+		r = requests.post(self.url_post)
+		self.assertLessEqual(r.elapsed.total_seconds(),self.timeout_post/1000)
 
 if __name__ ==  '__main__':
 	unittest.main()
