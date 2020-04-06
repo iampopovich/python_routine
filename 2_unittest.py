7554896caeb3b6c3f1eee424d451a2cb57c1ecfc
Alex <iampopovich@gmail.com>
2020-04-07 01:38:27 +0300

    2_unittest.py бил тесткейсы по логически обособленным классам. не нравится огород входных аргументов

--- 2_unittest.py
+++ 2_unittest.py
@@ -3,21 +3,13 @@ import re
 import inspect
 import unittest
 
-class TestREST(unittest.TestCase):
+class testResponseStatus(unittest.TestCase): #remember pep-8 and refactor it !
 
 	def setUp(self):
 		self.url = 'https://www.google.com'
 		self.timeout = 300
 		self.status = 2000
-		self.text = 'go00ogle'
-
-	def test_time_response_get(self):
-		r = requests.get(self.url)
-		self.assertLessEqual(r.elapsed.total_seconds(), self.timeout/1000 )
-
-	def test_time_response_post(self):
-		r = requests.post(self.url)
-		self.assertLessEqual(r.elapsed.total_seconds(),self.timeout/1000)
+		self.text = 'google'
 
 	def test_response_status(self):
 		r = requests.get(self.url)
@@ -28,19 +20,21 @@ class TestREST(unittest.TestCase):
 		contains = re.search(self.text,r.text)
 		self.assertIsNotNone(contains)
 
-	# def runTest(self):
-	# 	self.check_response_status()
-	# 	self.check_response_contains()
+class testResponseTime(unittest.TestCase):
+# Remember DRY and refactore it !
+	def setUp(self):
+		self.url = 'https://www.google.com'
+		self.timeout = 300
+		self.status = 200
+		self.text = 'google'
 
+	def test_time_response_get(self):
+		r = requests.get(self.url)
+		self.assertLessEqual(r.elapsed.total_seconds(), self.timeout/1000 )
 
-# test_get = check_time_response_get('https://www.goog0le.com', 300)
-# test_status = check_response_status('https://www.goo--gle.com', 300)
-# test_contains = check_response_contains('https://www.google.com', 'go0gle')
-
-# print(test_get, '\n')
-# print(test_status,'\n')
-# print(test_contains,'\n')
+	def test_time_response_post(self):
+		r = requests.post(self.url)
+		self.assertLessEqual(r.elapsed.total_seconds(),self.timeout/1000)
 
 if __name__ ==  '__main__':
-	# unittest.TextTestRunner().run(TestREST())
 	unittest.main()
