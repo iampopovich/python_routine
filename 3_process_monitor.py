cabfa21d02fefdfd4bedbc9478309c64acaccede
Alex <iampopovich@gmail.com>
2020-04-07 20:37:49 +0300

    3_process_monitor.py вроде более менее рабочий монитор. терминатор из него такой себе. но для юнит-тестирования сойдет

--- 3_process_monitor.py
+++ 3_process_monitor.py
@@ -2,23 +2,36 @@ import os
 import sys
 import argparse
 import re
+import psutil
 
 def init_argparser():
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-p', metavar = 'PROCESS_NAME', type = 'str',
-		help = 'укажи имя или часть имени процесса', required=True)
-	parser = argparse.add_argument('-d', metavar = 'DESTROY', action = 'store_true')
-	parser = argparse.add_argument('-s', metavar = 'SEARCH', action = 'store_true')
+	parser.add_argument('-p', metavar = 'PROCESS_NAME', type = str ,help = 'укажи имя или часть имени процесса', required=True)
+	parser.add_argument('-k', help = 'set it if killing process is necessary', action = 'store_true')
+	parser.add_argument('-s', help = 'set it if process activity alerting is necessary', action = 'store_true')
 	return parser
 
 def kill_process(name):
-	return None
+	while True:
+		for proc in psutil.process_iter():
+			if name.lower() in proc.name().lower():
+				proc.terminate()
 
 def search_process(name):
-	return None
+	while True:
+		for proc in psutil.process_iter():
+			try:
+				if name.lower() in proc.name().lower():
+					print('Process {} now running wit PID: {}'.format(name, proc.pid))
+			except Exception as ex:
+				print (ex)
+				return None
 
 def main():
 	parser = init_argparser()
 	args = parser.parse_args()
 	if args.d: kill_process(args.p)
 	if args.s: search_process(args.p)
+
+if __name__ == '__main__':
+	main()
