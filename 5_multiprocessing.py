e9cac91077be006be50559d0db1fea3598b3d17b
Alex <iampopovich@gmail.com>
2020-04-10 16:07:57 +0300

    5_multiprocessing.py коммит драфта . stage commit

--- 5_multiprocessing.py
+++ 5_multiprocessing.py
@@ -1,1 +1,27 @@
 import multiprocessing
+import math
+import argparse
+import time
+
+def init_parser():
+	parser = argparse.ArgumentParser()
+	parser.add_argument()
+	return parser
+
+def timeit(func):
+	def wrapper(*args,**kwargs):
+		t1 = time.time()
+		func()
+		t2 = t1 - time.time()
+		primt(t2)
+	return wrapper
+
+@timeit
+def worker():
+	return None
+
+def main():
+	return None
+
+if __name__ == '__main__':
+	main()
