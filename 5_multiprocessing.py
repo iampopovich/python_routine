bfa8ac266a71269e82080a2fc7636eb47faeb202
Alex <iampopovich@gmail.com>
2020-04-12 16:38:26 +0300

    5_multiprocessing.py готово для сравнения с тридами. будут еще мелкие правки. мб сделаю имплементацию для вызова из автотеста - но это не точно

--- 5_multiprocessing.py
+++ 5_multiprocessing.py
@@ -1,38 +1,51 @@
-import multiprocessing
-import math
+from multiprocessing import Process, cpu_count
 import argparse
-import time
-import random
+from time import time
+from random import random
 
 def init_parser():
-	cores = multiprocessing.cpu_count()
+	cores = cpu_count()
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-b', metavar='BLOCK_SIZE', type = int, required = True)
 	parser.add_argument('-p', metavar = 'PROC_NUM', type = int, choices=(range(1,cores+1)), required=True)
 	return parser
 
+def init_slicer(block, size):
+	out = []
+	chunk = block.__len__()//size
+	for _ in range(0,size):
+		if _ == size - 1: 
+			out.append(block)
+		else:
+			out.append(block[:chunk])
+			block = block[chunk:]
+	return out
+
 def timeit(func):
 	def wrapper(*args,**kwargs):
-		t1 = time.time()
-		func()
-		t2 = t1 - time.time()
-		primt(t2)
+		t1 = time()
+		func(*args,**kwargs)
+		t2 = time() - t1
+		print(t2)
 	return wrapper
 
 @timeit
-def worker():
+def worker(chunk):
+	chunk = list(map(lambda x: x**2, chunk))
 	return None
 
 def main():
 	parser = init_parser()
 	args = parser.parse_args()
-	block = [random.random() for i in range(args.b)]
+	block = [random() for i in range(args.b)]
 	chunks = init_slicer(block, args.p)
 	procs = list()
-	# for ch in chunks:
-		# p = multiprocessing.Process()
-		# procs.append(p)
-		# p._Popen()
+	for ch in chunks:
+		p = Process(target = worker, args=(ch,))
+		procs.append(p)
+		p.start()
+	for p in procs:
+		p.join()
 	return None
 
 if __name__ == '__main__':
