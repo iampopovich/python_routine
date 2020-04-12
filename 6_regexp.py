855bc85eab4483c78bd45c0480c28de4f10c256f
Alex <iampopovich@gmail.com>
2020-04-12 22:20:22 +0300

    6_regexp.py фиксы регекспов для униттеста

--- 6_regexp.py
+++ 6_regexp.py
@@ -5,30 +5,39 @@ import unittest
 
 class regexTestcase(unittest.TestCase):
 
-	def test_starts_with(self, text, part):
-		# result = re.match(r'{}'.format(part), text)
-		result = re.search(r'^{}'.format(part), text)
-		assertTrue(result)
+	# def __init__(self, *args,**kw):
+	# 	# super().__init__(*args)
+	# 	self.text = None
+	# 	self.regex = None
+	# 	self.count = None
+	def setUp(self):
+		self.text = 'hehehehhhehhehhe'
+		self.part = 'he'
+		self.count = 7
 
-	def test_ends_with(self,text,part):
-		result = re.search(r'{}$'.format(part), text)
-		assertTrue(result)
+	def test_starts_with(self):
+		result = re.search(r'^{}'.format(self.part), self.text)
+		self.assertTrue(result)
 
-	def test_match_count_less(self, text, part, count):
-		result = re.findall(r'{}'.format(part), text)
-		assertLess(len(result), count)
+	def test_ends_with(self):
+		result = re.search(r'{}$'.format(self.part), self.text)
+		self.assertTrue(result)
 
-	def test_match_count_equal(self, text, part, count):
-		result = re.findall(r'{}'.format(part), text)
-		assertEqual(len(result), count)
+	def test_match_count_less(self):
+		result = re.findall(r'{}'.format(self.part), self.text)
+		self.assertLess(len(result), self.count)
 
-def test_match_count_greater(self, text, part, count):
-		result = re.findall(r'{}'.format(part), text)
-		assertGreater(len(result), count)
+	def test_match_count_equal(self):
+		result = re.findall(r'{}'.format(self.part), self.text)
+		self.assertEqual(len(result), self.count)
 
-		 
-def main():
-	return None
+	def test_match_count_greater(self):
+		result = re.findall(r'{}'.format(self.part), self.text)
+		self.assertGreater(len(result), self.count)
 
 if __name__ == '__main__':
-	main()
+	# test = regexTestcase()
+	# test.text = 'hehehehhhehhehhe'
+	# test.part = 'he'
+	# test.count = 2
+	unittest.main()
