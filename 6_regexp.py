2d1512124d30328370b857e0bb985d1eb60f26ae
Alex <iampopovich@gmail.com>
2020-04-12 20:21:36 +0300

    stage commit

--- 6_regexp.py
+++ 6_regexp.py
@@ -1,1 +1,34 @@
+import re
 import threading
+import unittest
+
+
+class regexTestcase(unittest.TestCase):
+
+	def test_starts_with(self, text, part):
+		# result = re.match(r'{}'.format(part), text)
+		result = re.search(r'^{}'.format(part), text)
+		assertTrue(result)
+
+	def test_ends_with(self,text,part):
+		result = re.search(r'{}$'.format(part), text)
+		assertTrue(result)
+
+	def test_match_count_less(self, text, part, count):
+		result = re.findall(r'{}'.format(part), text)
+		assertLess(len(result), count)
+
+	def test_match_count_equal(self, text, part, count):
+		result = re.findall(r'{}'.format(part), text)
+		assertEqual(len(result), count)
+
+def test_match_count_greater(self, text, part, count):
+		result = re.findall(r'{}'.format(part), text)
+		assertGreater(len(result), count)
+
+		 
+def main():
+	return None
+
+if __name__ == '__main__':
+	main()
