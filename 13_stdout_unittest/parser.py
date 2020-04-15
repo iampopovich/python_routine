db02d8db6182139a49fc2a1190f0f6dbcaa61104
Alex <iampopovich@gmail.com>
2020-04-16 00:35:34 +0300

    13_stdout_unittest/ какой-то реальный факап с тридами/подпроцессом. не понимаю где лок, стдаут читается только после завершения Popen.  попробую call

--- 13_stdout_unittest/parser.py
+++ 13_stdout_unittest/parser.py
@@ -7,14 +7,19 @@ import unittest
 #АССЕРТ true НА if match in line 
 
 def parser(proc, match = 100):
-	for line in proc.stdout:
-		if match in line:
-			print('{} FOUND!'.format(match))
-
+	while True:
+		line = proc.stdout.readline()
+		if line:
+			if match in line:
+				print('{} FOUND!'.format(match))
+		if line.__len__() == 0 and proc.poll() is not None:
+			break 
+	rc = proc.poll()
+	return rc
 
 def main():
-	# proc = subprocess.Popen(['/bin/sh','-c','{}/generator.sh'.format(os.getcwd())],stdout=subprocess.PIPE, shell = True)
-	proc = subprocess.Popen(['/bin/sh','echo 100'],shell=True, stdout = subprocess.PIPE)
+	proc = subprocess.Popen(['/bin/sh','-c','{}/generator.sh'.format(os.getcwd())],stdout=subprocess.PIPE, shell = True, encoding = 'utf-8')
+	# proc = subprocess.Popen(['/bin/sh','echo 100'],shell=True, stdout = subprocess.PIPE)
 	t = threading.Thread(target = parser, args=(proc,'3',), daemon=True)
 	t.start()
 	t.join()
