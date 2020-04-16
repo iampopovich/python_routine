db7114de7f930dc2830443b6aa3f56a00b334f45
Alex <iampopovich@gmail.com>
2020-04-16 18:36:40 +0300

    7_logging.py оформил init_slicer по красоте с логированием уровня ошибок и инфо. ну в целом не так сложно. Логирование сейчас я бы применял в отдельный файл, как применял в модуле для ОКБ (спойлер , там я колхолиз с записью в sqlite базенку, мол так удобней вести статистику, ошибки тоже туда же писал - это фейл).

--- 7_logging.py
+++ 7_logging.py
@@ -10,19 +10,22 @@ def init_argparser():
 	return parser
 
 def init_slicer(block, size):
-	logging.basicConfig(level = logging.INFO)
-	logging.info('started slicer initialization...')
-	out = []
-	chunk = block.__len__()//size
-	for _ in range (0, size):
-		if _ == size - 1: 
-			out.append(block)
-		else:
-			out.append(block[:chunk])
-			block = block[chunk:]
-		logging.info('slice {} appended...'.format(_+1))
-	logging.info('slicing finished successful!')
-	return out
+	try:
+		logging.basicConfig(level = logging.INFO)
+		logging.info('started slicer initialization...')
+		out = []
+		chunk = block.__len__()//size
+		for _ in range (0, size):
+			if _ == size - 1: 
+				out.append(block)
+			else:
+				out.append(block[:chunk])
+				block = block[chunk:]
+			logging.info('slice {} appended...'.format(_+1))
+		logging.info('slicing finished successful!')
+		return out
+	except Exception as ex:
+		logging.error('slicer returned exception: \n{}'.format(ex))
 
 def worker(data):
 	data = list(map(lambda x: x+1 , data))
