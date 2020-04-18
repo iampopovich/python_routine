980f178ad6d979c4de38061cd7c4062dfeccb7e2
Alex <iampopovich@gmail.com>
2020-04-18 21:53:45 +0300

    9_sockets/ stage commit

--- 9_sockets/9_socket_server.py
+++ 9_sockets/9_socket_server.py
@@ -1,1 +1,22 @@
+import sockets
 
+def init_server():
+	sock = sockets.socket()
+	sock.bind('',9090)
+	sock.listen(1)
+	return socket.accept()
+
+def run_server(connection, address):
+	while True:
+		data = con.recv(1024)
+		if not data: break
+		if '--help' in data: show_help(conn)
+		#conn.send(data.upper())
+	conn.close()
+
+def main():
+	conn, addr = init_server()
+	run_server()
+
+if __name__ == '__main__':
+	main()
