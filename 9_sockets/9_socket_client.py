191e732cb4b2aaf5b706b471bed8d0bf3ef4809a
Alex <iampopovich@gmail.com>
2020-04-19 22:23:09 +0300

    stage commit

--- 9_sockets/9_socket_client.py
+++ 9_sockets/9_socket_client.py
@@ -1,1 +1,20 @@
-import sockets
+import socket
+
+def init_client():
+	sock = socket.socket()
+	sock.connect(('',9191))
+	
+	while True:
+		info = input('write something... ')
+		if info == 'exit': break
+		else:
+			sock.send(info)
+			data = sock.recv(1024)
+
+	sock.close()
+
+def main():
+	init_client()
+
+if __name__:=='__main__':
+	main()
--- 9_sockets/9_socket_server.py
+++ 9_sockets/9_socket_server.py
@@ -1,22 +1,20 @@
-import sockets
+import socket
 
 def init_server():
-	sock = sockets.socket()
-	sock.bind('',9090)
+	sock = socket.socket()
+	sock.bind(('',9191))
 	sock.listen(1)
-	return socket.accept()
-
-def run_server(connection, address):
+	conn, addr = sock.accept() 
+	print('server started...')
 	while True:
-		data = con.recv(1024)
+		data = connnection.recv(1024)
 		if not data: break
 		if '--help' in data: show_help(conn)
 		#conn.send(data.upper())
 	conn.close()
 
 def main():
-	conn, addr = init_server()
-	run_server()
+	init_server()
 
 if __name__ == '__main__':
 	main()
