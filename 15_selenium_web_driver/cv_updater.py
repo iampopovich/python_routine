c7dfb86d3967aa688bd0d59476b355dbc7013ae9
Alex <iampopovich@gmail.com>
2020-04-21 02:57:06 +0300

    15_selenium_web_driver init

---
+++ 15_selenium_web_driver/cv_updater.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import json
+import time
+
+def get_config():
+	with open('cred.cfg', 'r') as c:
+		config = json.load(c)
+	return config
+
+def init_browser():
+	
+	return browser
+
+def autorize(browser):
+	return browser
+
+def update_cv(browser,url):
+
+
+def main():
+	return None
+
+if __name__ == '__main__':
+	main()
