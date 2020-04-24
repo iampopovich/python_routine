054192e129b582951c343e6e51c595cfc33c733c
Alex <iampopovich@gmail.com>
2020-04-24 14:34:15 +0300

    13_stdout_unittest в потоки не получилось, в процессы удалось через системный стдаут чекать вывод процесса. НЕКСТ СТЕП - передать ИД процесса в утилиту по типу strace -e trace=open -p 22254 -s 80 -o output.txt

--- 13_stdout_unittest/parser.py
+++ 13_stdout_unittest/parser.py
@@ -1,54 +1,30 @@
 import os
 import sys
 import subprocess
-import threading
-import unittest
 import logging
-import random
-import time
 
-
-# имеет смысл описать парсер классом
-
-
-def writer(file):
-	while True:
-		with open(file,'a') as f:
-			f.write('{}\n'.format(random.randint(0,10)))
-		time.sleep(1)
-
-def parser_proc(proc, match = '100'):
+def parser_proc(command, file, proc_id = None):
+	proc = subprocess.Popen([command],stdout=subprocess.PIPE, shell = True, encoding = 'utf-8')	
 	while True:
 		line = proc.stdout.readline()
 		if line:
-			if match in line:
-				print('{} FOUND!'.format(match))
+			sys.stdout.write(line)
+			sys.stdout.flush()
+			with open(file,'a') as out:
+	 			out.write('{}'.format(line))
 		if line.__len__() == 0 and proc.poll() is not None:
 			break 
-	rc = proc.poll()
-	return rc
 
-def parser_file(file,match = '100'):
-	while True:
-		try:
-			with open(file,'r') as f:
-				if match in f.readlines()[-1:]:
-					print('{} was found!'.format(match))#мб вот здесь лок аута
-					# sys.exit()
-		except Exception as ex:
-			logging.error('{}'.format(ex))
-			continue
+def get_process_id():
+	return None
 
 def main():
 	file = 'output.log'
-	threads = []
-	# proc = subprocess.Popen(['/bin/sh','-c','{}/generator.sh'.format(os.getcwd())],stdout=subprocess.PIPE, shell = True, encoding = 'utf-8')
-	t_writer = threading.Thread(target = writer, args=(file,),daemon = True)
-	t_parser = threading.Thread(target = parser_file, args=(file,'3',), daemon=True)
-	threads.extend([t_writer,t_parser])
-	for t in threads: t.start()
-	for t in threads: t.join()
+	command =  '. {}/generator.sh'.format(os.getcwd())
+	proc_id = get_process_id()
+	parser_proc(command,file)
+	
+	# parser_proc(command)
 
-		
 if __name__ == '__main__':
 	main()
