b6c9cbcb098d1340e0e42d6c3fb42ecd9540099b
Alex <iampopovich@gmail.com>
2020-04-30 23:02:09 +0300

    stage

---
+++ 15_selenium_web_driver/cv_updater_cli.py
@@ -0,0 +1,8 @@
+import requests
+import bs4
+
+def main():
+	return None
+
+if __name__ == '__main__':
+	main()
--- 17_log_splitter/Splitter.py
+++ 17_log_splitter/Splitter.py
@@ -9,6 +9,7 @@ class Splitter():
 		self.application = None
 		self.level = None
 		self.path_original_log = None
+		self.set_log_file()
 
 	def set_level(self, level = None):
 		if level:
@@ -28,8 +29,18 @@ class Splitter():
 		else:
 			self.path_original_log = input('drop original log file into terminal: ')
 
-	def split_file(self, apps = None, levels = None):
-
+	def split_file(self, app = None, level = None):
+		buf = []
+		lines = []
+		with open(self.path_original_log, 'r') as log:
+			lines = log.readlines()
+		for line in lines:
+			if re.search(r'^\[',line): #regexp for app
+				with open('{}.log'.format(app),'a') as out_app:
+					out_app.write(line)
+			if re.search(r'^\[',line): #regexp for level
+				with open('{}.log'.format(level),'a') as out_level:
+					out_level.write(line)
 		return None
 
 def init_argparser():
@@ -39,11 +50,12 @@ def init_argparser():
 	return parser
 
 def main():
-	# parser
+	argparser = init_argparser()
+	args.parse_args()
 	split = Splitter()
-	split.set_level() #pass level argument
-	split.set_app()
-	split.set_log_file()
+	split.set_level(args['level']) if args['level'] else split.set_level()
+	split.set_app(args['app']) if args['app'] else split.set_app()
+	split.set_log_file(args['file']) if args['file'] else split.set_log_file()
 	split.split_file()
 
 if __name__ == '__main__':
