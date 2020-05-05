e1690cc1c5dd714cadeb2d5f7c2a10caaf34a840
Alex <iampopovich@gmail.com>
2020-05-05 16:28:30 +0300

    stage

--- 18_bs4.py
+++ 18_bs4.py
@@ -20,16 +20,27 @@ class Scrapper:
 		self.search_engine = None
 		self.user_agent = user_agent.generate_user_agent()
 		self.url_target = None
+		self.keywords = None
+		self.level = 2
 
-	def set_set_url_target(self,url):
+	def set_url_target(self,url):
 		# target = re.search(r"baehr.ru|baehrrussia|behrru|podologclub|nautilus2000|uchebnyj_tsentr_nautilus|pedicurenautilus|pedicure_nautilus|www.krasota.spb.ru",resp)
 		self.url_target = url
+
+	def set_level(self,level):
+		self.level = level
 
 	def init_scrapper(self, engine, queries):
 		for i,query in enumerate(queries):
 			time.sleep(30)
 			results = []
-			response = search(query, tld="co.in", num=80, start = 0, stop=79, pause=70, user_agent = uagent)
+			response = search(query, 
+								tld="co.in", 
+								num=80, 
+								start = 0,
+								stop=79, 
+								pause=70, 
+								user_agent = uagent)
 			for position,resp in enumerate(response):
 				if target is None: continue
 				else:
@@ -39,25 +50,55 @@ class Scrapper:
 				output.write("%s\n"%("\n".join(results)))
 			glob_index_google+=1
 
-	def generate_requests_from_keywords(keywords): 
-		return keywords.permutation(4)
+	def set_keywords(self, keywords):
+		result = []
+		for _ in range(n):
+			result.extends(itertools.permutations(keywords,self.level)) 
+		self.keywords = result
 
-	def generate_requests_from_file(file):
-		keywords = []
+	def set_keywords_from_file(self, file):
+		result = []
 		with open(file, 'r') as f:
 			json_load = json.load(f)
 			keywords = json_load['keywords']
-		return keywords.permutation(4)
+		for _ in range(n):
+			result.extends(itertools.permutations(keywords,self.level)) 
+		self.keywords = result
 
 	def get_config():
 		return None #parsed json
 
 def init_argparser():
-	return None
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-kwf', 
+						metavar = 'KEYWORDS_FILE', 
+						action = 'store_true', 
+						help = 'укажи имя или часть имени процесса')
+	parser.add_argument('-kw', 
+						help = '', 
+						type = str)
+	parser.add_argument('-se',
+						help = 'choose search engine', 
+						type = str, 
+						choices = ['google','yandex','duckduck'])
+	parser.add_argument('-pl', 
+						metavar = 'PERMUTATION_LEVEL',
+						type = int,  
+						help = 'how many permutations you want to check', 
+						action = 'store_true')
+	parser.add_argument('-tg',
+						metavar = 'TARGET_MASK',
+						type = str,
+						help = 'type mask of target source you want to find',
+						required = True)
+	return parser
 
 def main():
-	argparser = init_argparser()
+	parser = init_argparser()
+	args = parser.parse_arguments()
 	scrapper = Scrapper()
+	if args['pl']: scrapper.set_level(args['pl'])
+	if args['tg']
 	scrapper.generate_requests(keywords)
 
 	while True:
