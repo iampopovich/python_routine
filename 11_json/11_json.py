f2ac9cf50213643040eed039eed76408201dc79c
Alex <iampopovich@gmail.com>
2020-06-20 00:01:07 +0300

    11_json updated

--- 11_json/11_json.py
+++ 11_json/11_json.py
@@ -1,6 +1,11 @@
 import json
 
-default_data = {'key':'value'}
+default_data = {
+    "president": {
+        "name": "Zaphod Beeblebrox",
+        "species": "Betelgeusian"
+    }
+}
 
 def write_test_file(file = 'test_file.json', data = default_data):
 	with open(file, 'w') as input_file:
@@ -14,10 +19,17 @@ def upload_data_from_file(file = 'test_file.json'):
 	except Exception as ex:
 		return ex
 
+def show_data_prettify(data = default_data, indents = 4):
+	return json.dumps(data, indent = indents)
 
 def main():
 	write_test_file()
 	print(upload_data_from_file())
+	print(show_data_prettify())
+	print(show_data_prettify(indents = 1))
+
+
+
 
 if __name__ == '__main__':
 	main()
