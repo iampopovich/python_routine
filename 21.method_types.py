b5ad8b3f16b0d7d4aae72e16e3b89d9e9bfd3977
Alex <iampopovich@gmail.com>
2020-07-23 12:45:31 +0300

    tried defferent method types

---
+++ 21.method_types.py
@@ -0,0 +1,21 @@
+class toyClass:
+	def __init__(self, name):
+		self.name = None
+
+	def instance_method(self):
+		return 'instance method called', self
+	
+	@classmethod
+	def class_method(cls):
+		return 'class method called', cls
+
+	@staticmethod
+	def static_method():
+		return 'static method called'
+
+
+toy = toyClass('heher')
+
+print(toy.instance_method())
+print(toyClass.class_method())
+print(toyClass.static_method())
