a763035188647f5959563ed85baedc3d0e6cb604
Alex <iampopovich@gmail.com>
2020-08-27 19:10:21 +0300

    minor fix

--- 23.nxopen_refactoring/7_check_pmi_objects.py
+++ 23.nxopen_refactoring/7_check_pmi_objects.py
@@ -8,17 +8,17 @@ class NXJournal:
 		self.lw = self.session.ListingWindow
 		self.PMIs = self.work_part.PmiManager.Pmis
 
-def find_pmi_objects(self):
-	try:
-		if self.PMIs:
-			pmi_list = [item.JournalIdentifier for item in self.PMIs]
-			out_string = "Модель содержит объекты PMI \n{}".format(
-				"\n".join(map(str,pmi_list))
-				)
-		else: out_string = "Модель соответствует требованиям"
-		return out_string
-	except Exception as ex:
-		return("find_pmi_objects failed with {}".format(ex))
+	def find_pmi_objects(self):
+		try:
+			if self.PMIs:
+				pmi_list = [item.JournalIdentifier for item in self.PMIs]
+				out_string = "Модель содержит объекты PMI \n{}".format(
+					"\n".join(map(str,pmi_list))
+					)
+			else: out_string = "Модель соответствует требованиям"
+			return out_string
+		except Exception as ex:
+			return("find_pmi_objects failed with {}".format(ex))
 
 def main() :
 	app = NXJournal()
