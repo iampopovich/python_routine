2bf73e9b1d0564e148db995424b70b3ae5d6c746
Alex <iampopovich@gmail.com>
2020-08-29 19:14:01 +0300

    code style improvements and global renaming + using pep8 autoformatter

---
+++ 23.nxopen_refactoring/8_check_background_color.py
@@ -0,0 +1,62 @@
+import NXOpen
+import NXOpen.UF
+import subprocess
+import sys
+
+
+class NXJournal:
+
+	def __init__(self):
+		self.session = NXOpen.Session.GetSession()
+		self.work_part = self.session.Parts.Work
+		self.lw = self.session.ListingWindow
+		self.uf_session = NXOpen.UF.UFSession.GetUFSession()
+		self.python_path = "D:\\Programs\\Python\\Python35\\python.exe"
+		self.script_path = "C:\\Users\\PopovAV\\Desktop\\tempWF\\Разработка\\_test_7-5-2-DONE_checkPixels.py"
+		self.save_part_image_path = "C:\\temp\\view-{}.jpg"
+		lw.Open()
+
+	def check_background_color(self):
+		work_views = self.work_part.ModelingViews
+		bad_views = []
+		for i, item in enumerate(work_views):
+			layout = self.work_part.Layouts.FindObject("L1")
+			layout.ReplaceView(
+				self.work_part.ModelingViews.WorkView, item, True)
+			item.Orient(item.Name, NXOpen.View.ScaleAdjustment.Fit)
+			self.uf_session.Disp.CreateImage(
+				self.save_part_image_path.format(i),
+				self.uf_session.Disp.ImageFormat.BMP,
+				self.uf_session.Disp.BackgroundColor.ORIGINAL
+			)
+			sub_proc = subprocess.Popen(
+				[
+					self.python_path.format(i),
+					self.script_path,
+					self.save_part_image_path
+				]
+			)
+			sub_proc.wait()
+			bad_color_flag = sub_proc.communicate()
+			sub_proc.kill()
+			self.lw.WriteLine(str(bad_color_flag))
+			if bad_color_flag:
+				bad_views.append(item.Name)
+		if len(bad_views) == 0:
+			return "Закраска фона соответствует требованиям"
+		elif len(bad_views) == 1:
+			return "Закраска фона {} не соответствует требованиям".format(
+				"\n".join(bad_views))
+		elif len(bad_views) > 1:
+			return "Закраска фона не соответствует требованиям:\n{}".format(
+				"\n".join(bad_views))
+
+
+def main():
+	app = NXJournal()
+	app.lw.Open()
+	app.lw.WriteLine(app.check_background_color())
+
+
+if __name__ == "__main__":
+	main()
