7750b25fb4b76f7b6eefe7bcd1dcf3e06b9f9550
Alex <iampopovich@gmail.com>
2020-09-10 01:46:16 +0300

    remote controller v1.5

--- 29_remote_controller/socket_server.py
+++ 29_remote_controller/socket_server.py
@@ -1,68 +1,42 @@
-import socket
 import os
 import sys
 import json
 import re
-
-
-HTML_PAGE_TEMPLATE = '<!DOCTYPE HTML > \
-        <html >\
-        <head >\
-        <meta charset = "utf-8" >\
-        <title > Тег BUTTON </title >\</head >\
-    <body >\
-    <form>\
-       REPLACE_IT\
-    </form>\
-    </body >\
-</html >'
+import http.server
+import socketserver
 
 
 def parse_json_commands(file):
     with open(file, 'r') as f:
         config = json.load(f)
     return config['buttons']
-        # buttons - object(title, actopn)
 
+def get_html_template(path):
+    with open(path, 'r') as html:
+        content = html.read()
+    return content
 
 def generate_html(file):
+    html_template = get_html_template('./index_template.html')
     config = parse_json_commands(file)
     buttons = []
     button_html = '<input type="button" value="{}" onclick="{}">'
     for item in config:
         buttons.append(button_html.format(item['title'], item['action']))
-    return re.sub(r'REPLACE_IT', '\n'.join(buttons), HTML_PAGE_TEMPLATE)
+    return re.sub(r'REPLACE_IT', '\n'.join(buttons), html_template)
 
 
-def init_server():
-    sock = socket.socket()
-    sock.bind(('', 9090))
-    sock.listen(1)
-    conn, addr = sock.accept()
-    return conn, addr
-
-
-def run_server(connection):
-    try:
-        print('server started...')
-        while True:
-            data = connection.recv(1024)
-            if not data:
-                break
-            os.system('cmd {}'.format(data))
-            connection.send(data.upper())
-
-    except Exception as ex:
-        connection.close()
-        raise ex
-    finally:
-        connection.close()
+def run_server():
+    PORT = 8080
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
 
 
 def main(args):
     print(generate_html(args[1]))
-    # conn, addr = init_server()
-    # run_server(conn, addr)
+    run_server()
 
 
 if __name__ == '__main__':
