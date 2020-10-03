a783df867b7ec6d0f58e7fa54cf8049e9d33683e
Alex <alex.popov@zodiacsystems.com>
2020-10-03 22:38:45 +0300

    clover recoder deployed for usage

---
+++ clover_recorder.py
@@ -0,0 +1,31 @@
+import sys
+from ppadb.client import Client as AdbClient
+import time
+
+
+def record_clover_demo(ip):
+    client = AdbClient(host="127.0.0.1", port=5037)
+    device = client.device(ip)
+    if not device:
+        print("problem with connection")
+        return
+    root_dir_name = "/sdcard/demo_clover"
+    device.shell("adb shell mkdir {}".format(root_dir_name))
+    demo_dir_name = "{}/{}".format(root_dir_name, time.time())
+    device.shell("adb shell mkdir {}".format(demo_dir_name))
+    counter = 0
+    while True:
+        screenshot_name = "{}/screenshot_{}.png".format(demo_dir_name, counter)
+        device.shell("adb shell screencap -p {}".format(screenshot_name))
+        counter+=1
+        print("DEBUG SCREENSHOTER : {} was created".format(screenshot_name))
+        time.sleep(0.5)
+
+def main(args):
+    record_clover_demo(args[1])
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
+# ffmpeg -framerate 5 -start_number 0 -i Desktop/1601752058.8383842/screenshot_%d.png -vcodec mpeg4 Desktop/test.mp4
