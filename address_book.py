842bc5f4050bc34cec0e582a76b9b1290775af25
Alex <alex.popov@zodiacsystems.com>
2020-10-06 02:22:07 +0300

    adress book solution

---
+++ address_book.py
@@ -0,0 +1,17 @@
+# d = dictionary
+
+person1 = {'name' : {'last_name' : "Пушкин", 'first_name' : "Александр", 'middle_name' : "Сергеевич"}, 'DOB': 26/05/1799, 'phone' : 8-967-345-23-41}}
+person2 = {'name' : {'last_name' : "Маяковский", 'first_name' : "Владимир", 'middle_name' : "Владимирович"}, 'DOB': 19/07/1893, 'phone' : 8-911-321-35-90}}
+person3 = {'name' : {'last_name' : "Ахматова", 'first_name' : "Анна", 'middle_name' : "Андреевна}, 'DOB': 23/06/1889, 'phone' : 8-981-943-84-82}}
+person4 = {'name' : {'last_name' : "Бродский", 'first_name' : "Иосиф", 'middle_name' : "Александрович"}, 'DOB': 24/05/1940, 'phone' : 8-923-745-23-45}}
+person5 = {'name' : {'last_name' : "Цветаева", 'first_name' : "Марина", 'middle_name' : "Ивановна"}, 'DOB': 08/10/1892, 'phone' : 8-956-672-84-89}}
+
+
+a = input("Что делаем? ")
+
+d = int(input("0 - добавить контакт"
+              "1 - просмотр контактов"
+              "2 - поиск по имени"
+              "3 - поиск по номеру"
+              "4 - удалить контакт"))
+if d ==1
