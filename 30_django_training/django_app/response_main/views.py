8b0d523f554e24070d7b661f9b9620a5e0640df9
Alex <alex.popov@zodiacsystems.com>
2020-11-11 02:04:20 +0300

    add views for help/about/index amazing

--- 30_django_training/django_app/response_main/views.py
+++ 30_django_training/django_app/response_main/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request='nothing'):
-    return HttpResponse("You asked me {}".format(request))
+def index(req):
+    return HttpResponse("<h2>Главная</h2>")
 
+
+def about(req):
+    return HttpResponse("<h2>Инфо</h2>")
+
+
+def help(req):
+    return HttpResponse("<h2>Помощь</h2>")
