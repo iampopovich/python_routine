9829f09ed34b6bff587b4f8bacf895257f867a2b
Alex <alex.popov@zodiacsystems.com>
2020-11-22 16:27:33 +0300

    fix for users url with name user_id

--- 30_django_training/django_app/django_app/urls.py
+++ 30_django_training/django_app/django_app/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     re_path(r'path', views.regular_middleware, name='reg_url'),
     re_path(r'^products/phones|tablets/', views.gadgets, name='gadgets'),
     re_path(r'^products/(?P<product_id>\d+)/', views.products),
-    re_path(r'^users/(?P<id>\d+)/(?P<user_name>\D+)/', views.users),
+    re_path(r'^users/(?P<user_id>\d+)/(?P<user_name>\D+)/', views.users),
 ]
