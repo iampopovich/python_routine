0bd6939d5131d794b23d89d69beb4147709c8574
Alex <alex.popov@zodiacsystems.com>
2020-12-06 17:38:49 +0300

    form fields placed in into form

--- 30_django_training/django_app/response_main/forms.py
+++ 30_django_training/django_app/response_main/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField()
     age = forms.IntegerField()
+    checkbox = forms.BooleanField()
+    selection = forms.NullBooleanField()
+    text_input = forms.CharField()
+    field_email = forms.EmailField()
+    field_ip = forms.GenericIPAddressField()
