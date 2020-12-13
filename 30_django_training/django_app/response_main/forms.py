be11f029f47dd96279f4c25081ea453ed4d9e9aa
Alex <alex.popov@zodiacsystems.com>
2020-12-13 17:14:05 +0300

    finishe with form validating

--- 30_django_training/django_app/response_main/forms.py
+++ 30_django_training/django_app/response_main/forms.py
@@ -22,17 +22,19 @@ class UserForm(forms.Form):
     field_datetime = forms.DateTimeField(required=False)
     field_duration = forms.DurationField(required=False)
     field_split_date_time = forms.SplitDateTimeField(required=False)
-    field_integer_value = forms.IntegerField()
-    field_decimal_value = forms.DecimalField()
-    field_float_value = forms.FloatField()
-    field_choice = forms.ChoiceField(
+    field_integer_value = forms.IntegerField(required=False)
+    field_decimal_value = forms.DecimalField(required=False)
+    field_float_value = forms.FloatField(required=False)
+    field_choice = forms.ChoiceField(required=False,
         choices=((0, 'zero'), (1, 'one'), (2, 'two')))
 
 
 class CustomForm(forms.Form):
     field_name = forms.CharField(
-        label="Name_label", initial="Username def", help_text="set your name")
+        label="Name_label", initial="Username def", help_text="set your name",
+        min_length=15, max_length=55)
     field_age = forms.IntegerField(
-        label="Age_label", initial=10000, help_text="set your age")
+        label="Age_label", initial=10000, help_text="set your age",
+        min_value=18, max_value=118)
     field_comment = forms.CharField(
         label="Comment_label", widget=forms.Textarea, initial="great comment")
