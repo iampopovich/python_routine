7ee0aea233bd3ffdc7ec55ab8ea12644001a2643
Alex <iampopovich@gmail.com>
2020-12-21 21:17:30 +0300

    add handler for root route

--- 30_django_training/simple_site/simple_site/settings.py
+++ 30_django_training/simple_site/simple_site/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'styled_app',
+    'root_app',
 ]
 
 MIDDLEWARE = [
---
+++ 30_django_training/simple_site/styled_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StyledAppConfig(AppConfig):
+    name = 'styled_app'
