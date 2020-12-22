ecf8f6c3c1df3103515973d89a55ec0bc1209d5a
Alex <iampopovich@gmail.com>
2020-12-22 06:04:16 +0300

    added dynamic render for contacts

--- 30_django_training/simple_site/root_app/views.py
+++ 30_django_training/simple_site/root_app/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-	return render(request, 'root_app/main_page.html', {})
+    return render(request, 'root_app/main_page.html', {})
+
+
+def contact(request):
+    return render(request, 'root_app/contacts.html', {'content':[
+    	'my name', '+100 000 999']})
