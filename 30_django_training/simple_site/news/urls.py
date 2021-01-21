cef63d8d15ee15436f222cee788e2081effe4678
Alex <iampopovich@gmail.com>
2021-01-21 09:09:48 +0300

    catch exeprimental returning ListView for queryset from view

--- 30_django_training/simple_site/news/urls.py
+++ 30_django_training/simple_site/news/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path, include
 from django.views.generic import ListView, DetailView
 from news.models import Articles
+from . import views
 
 urlpatterns = [
     re_path(r'^$', ListView.as_view(
@@ -8,5 +9,6 @@ urlpatterns = [
         template_name='news/posts.html')),
     path(r'<int:pk>', DetailView.as_view(
         model=Articles,
-        template_name='news/post.html'))
+        template_name='news/post.html')),
+    re_path(r'^get_all_articles$', views.get_all_articles)
 ]
--- 30_django_training/simple_site/news/views.py
+++ 30_django_training/simple_site/news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from news.models import Articles
 
-# Create your views here.
+
+def get_all_articles(request):
+    return ListView.as_view(
+        queryset=Articles.objects.all().order_by("-date")[:20],
+        template_name='news/posts.html')(request)
